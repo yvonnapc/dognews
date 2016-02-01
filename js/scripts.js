741c187c6bb692e75d2a8d7acd8f9378cabe8d90
Simon Temple and Yvonna Contreras <simonctemple@live.com>
2016-02-01 13:51:45 -0800

    Add park about and hours

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-
+function dogPark(offLeash, doggieBags, fenced, parkSize){
+  this.offLeash = offLeash;
+  this.doggieBags = doggieBags;
+  this.fenced = fenced;
+  this.parkSize = parkSize;
+}
 
   $(document).ready(function() {
       $('#example-getting-started').multiselect();
