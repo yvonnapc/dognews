1d4a5c48167ae945baf8e044d8fe8a5dd77f10c1
Simon Temple and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-01 14:13:16 -0800

    Add passing spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,10 @@
-describe('dogPark
+describe('dogPark', function(){
+  it("will describe a park based on its features", function(){
+    var testPark = new dogPark("offLeash", "doggieBags", "fenced", "Large");
+    expect(testPark.offLeash).to.equal("offLeash");
+    expect(testPark.doggieBags).to.equal("doggieBags");
+    expect(testPark.fenced).to.equal("fenced");
+    expect(testPark.parkSize).to.equal("Large");
+  });
+
+});
