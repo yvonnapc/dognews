71cad374011917a8b80a64f373d5e675e28f3120
Simon Temple and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-01 16:15:36 -0800

    make size prototype test for small medium or large selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
-function dogPark(offLeash, doggieBags, fenced, parkSize){
+function dogPark(offLeash, doggieBags, fenced, parkSize) {
   this.offLeash = offLeash;
   this.doggieBags = doggieBags;
   this.fenced = fenced;
   this.parkSize = parkSize;
 }
 
-  $(document).ready(function() {
-      $('#example-getting-started').multiselect();
+dogPark.prototype.search = function() {
+  if (this.parkSize === "Large") {
+    return $(".large").append("Large")
+  } else if (this.parkSize === "Medium") {
+    return $(".medium").append("Medium")
+  } else if (this.parkSize === "Small") {
+    return $(".small").append("Small")
+  } else {
+
+  }
+}
+
+var offLeash = false;
+var doggyBags = false;
+var fenced = false;
+
+$(document).ready(function() {
+  $("form#parkForm").submit(function(event) {
+
+    // $(".offLeash").hide();
+    var offLeash = $('input[name=feature]:checked').val();
+    var doggieBags = $('input[name=feature]:checked').val();
+    var fenced = $('input[name=feature]:checked').val();
+    var parkSize = $("select#parkSize").val();
+    var parkProfile = new dogPark(offLeash, doggieBags, fenced, parkSize);
+    parkProfile.search();
+console.log(new dogPark(offLeash, doggieBags, fenced, parkSize));
+    event.preventDefault();
   });
+});
