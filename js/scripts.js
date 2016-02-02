2a1fcb9159e59806c3a1c8f68b455e7f88b6b5ea
Simon Temple and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-02 10:45:17 -0800

    Add functioning glyphicons to park features

--- js/scripts.js
+++ js/scripts.js
@@ -12,23 +12,61 @@ dogPark.prototype.search = function() {
     return $(".medium").toggleClass("backgroundBorder")
   } else if (this.parkSize === "Small") {
     return $(".small").toggleClass("backgroundBorder")
-  } 
+  }
 }
 
 var offLeash = false;
 var doggyBags = false;
 var fenced = false;
 
+// dogPark.prototype.offLeash = function() {
+//   if (this.offLeash === "true") {
+//     $(".offLeash").append("<span class='glyphicon glyphicon-tree-deciduous'></span>")
+//   } else if (this.doggieBags === "true") {
+//     $(".dogBag").append("<span class='glyphicon glyphicon-cloud'></span>")
+//   } else if (this.fenced === "true") {
+//     $(".noFence").append("<span class='glyphicon glyphicon-link'></span>")
+//   }
+// }
+/* tree/offleash=glyph1, cloud/dogbag=glyph2, link/fenced=glyph3 */
+dogPark.prototype.noLeash = function() {
+  if (this.offLeash === "true") {
+    return $(".glyph1").show(".glyph1")
+  }
+}
+
+dogPark.prototype.bringBags = function() {
+  if (this.doggieBags === "true") {
+    return $(".glyph2").show(".glyph2")
+  }
+}
+
+dogPark.prototype.noFenced = function() {
+  if (this.fenced === "true") {
+    return $(".glyph3").show(".glyph3")
+  }
+}
+
+// var size_pizza = $('input[name="feature"]').is(":checked");
+
+/*-------- business ---------*/
 $(document).ready(function() {
   $("form#parkForm").submit(function(event) {
     $(".backgroundBorder").toggleClass("backgroundBorder")
-    // $(".offLeash").hide();
-    var offLeash = $('input[name=feature]:checked').val();
-    var doggieBags = $('input[name=feature]:checked').val();
-    var fenced = $('input[name=feature]:checked').val();
+
+    $(".glyph1").hide();
+    $(".glyph2").hide();
+    $(".glyph3").hide();
+    var offLeash = $('input[name="offLeash"]:checked').val();
+    var doggieBags = $('input[name="doggieBags"]:checked').val();
+    var fenced = $('input[name="fenced"]:checked').val();
     var parkSize = $("select#parkSize").val();
     var parkProfile = new dogPark(offLeash, doggieBags, fenced, parkSize);
     parkProfile.search();
+    parkProfile.noLeash();
+    parkProfile.bringBags();
+    parkProfile.noFenced();
+
 console.log(new dogPark(offLeash, doggieBags, fenced, parkSize));
     event.preventDefault();
   });
